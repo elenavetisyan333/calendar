dfae71c2032d5f8f374dbfea96321b431f9abb49
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2023-03-28 01:47:36 +0400

    add useReducer to calendar and some changes to style

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react'
 import Days from './Days';
 
 function Calendar() {
@@ -7,40 +6,30 @@ function Calendar() {
     const years = [2018, 2019, 2020, 2021 ,2022, 2023, 2024, 2025, 2026, 2027, 2028];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    const [monthId, setMonthId] = useState(date.getMonth());
-    const [yearId, setYearId] = useState(years.indexOf(date.getFullYear()));
+    const [monthId, dispatchMonthId] = useReducer(useMonthReducer, {month: date.getMonth()} );
+    const [yearId, dispatchYearId] = useReducer(useYearReducer, {year: years.indexOf(date.getFullYear())} );
     const [selectedDay, setSelectedDay] = useState(date.getDate());
     const [selectedMonth, setSelectedMonth] = useState(months[date.getMonth()]);
     const [selectedYear, setSelectedYear] = useState(date.getFullYear());
 
-
-    function moveToPrevMonth(){
-        monthId > 0 ? setMonthId(monthId - 1) :  moveToPrevYear();
+    function useMonthReducer(state, action){
+        if(action.type == "moveToPrevMonth") return { month: state.month - 1 }
+        else if(action.type == "moveToNextMonth") return { month: state.month + 1 }
+        else if(action.type == "moveToLastMonthInPrevYear") return { month: months.length - 1 }
+        else if(action.type == "moveToFirstMonthInNextYear") return { month: 0 }
     }
 
-    function moveToNextMonth(){
-        monthId < months.length - 1 ? setMonthId(monthId + 1) : moveToNextYear();
-    }
-
-    function moveToPrevYear(){
-        setMonthId(months.length - 1);
-        yearId > 0 ? setYearId(yearId - 1) : setYearId(years.length - 1);
-    }
-    function moveToNextYear(){
-        setMonthId(0);
-        yearId < years.length - 1 ? setYearId(yearId + 1) : setYearId(0);
+    function useYearReducer(state, action){
+        if(action.type == "moveToPrevYear"){
+            dispatchMonthId({type: "moveToLastMonthInPrevYear"}) 
+            return state.year > 0 ? {year: state.year - 1} : {year: years.length - 1}
+        }
+        else if(action.type == "moveToNextYear"){
+            dispatchMonthId({type: "moveToFirstMonthInNextYear"})
+            return state.year < years.length - 1 ? {year: state.year + 1} : {year: 0}
+        }
     }
     
-
-
-
-    useEffect(() =>{
-        setMonthId(monthId);
-        setYearId(yearId);
-    },[monthId,yearId])
-
-
-
   return (
     <div className="content">
         <div className="info">
@@ -50,17 +39,17 @@ function Calendar() {
         <table className='calendar'>
             <thead>
                 <tr className='year'>
-                    <th>{years[yearId]}</th>
+                    <th>{years[yearId.year]}</th>
                 </tr>
                 <tr className='month'>
                     <th>
-                        <button onClick={moveToPrevMonth}> 
+                        <button onClick={() => { monthId.month > 0 ? dispatchMonthId({type: "moveToPrevMonth"}) : dispatchYearId( {type: "moveToPrevYear"} )} }> 
                             <i className="fa-solid fa-chevron-left fa-sm"></i> 
                         </button>
                     </th>
-                    <th className='monthName'>{months[monthId]}</th>
+                    <th className='monthName'>{months[monthId.month]}</th>
                     <th>
-                        <button onClick={moveToNextMonth}>
+                        <button onClick={() => { monthId.month < months.length - 1 ? dispatchMonthId({type: "moveToNextMonth"}) : dispatchYearId( {type: "moveToNextYear"} )} }>
                             <i className="fa-solid fa-chevron-right fa-sm"></i>
                         </button>
                     </th>
@@ -77,9 +66,9 @@ function Calendar() {
                     <td>F</td>
                     <td>S</td>
                 </tr>
-                <Days year={years[yearId]} 
+                <Days year={years[yearId.year]} 
                         months={months}
-                        monthId={monthId} 
+                        monthId={monthId.month} 
                         setSelectedDay={setSelectedDay} 
                         setSelectedMonth={setSelectedMonth} 
                         setSelectedYear={setSelectedYear} 
